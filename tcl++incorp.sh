8580e4b07bd9d2ae02293f99efec42dc5195138e
Ron Fox <fox@frib.msu.edu>
2024-09-09 09:40:38 -0400

    Update version of tcl++:
    * Update the comment
    * Make the most recently used version default so in the future
      automated verification actions can be added.

--- tcl++incorp.sh
+++ tcl++incorp.sh
@@ -11,12 +11,20 @@
 #  The idea is that libtcl/libexception could be or not be centrally
 #  installed, but if not, built from the export directory.
 #
-# Last used with libtclplus-v2.0-000
+# Last used with libtclplus-v4.3-002
 
-baseURL=https://git.nscl.msu.edu/daqdev/libtclplus.git
+baseURL=https://github.com/FRIBDAQ/libtclplus.git
+
+default_tag="libtclplus-v4.3-002"
 
 tag="$1"
 uri="$2"
+
+if [ "$tag" = ""  ]
+then
+    tag=${default_tag}
+fi
+
 
 if [ "$uri" = "" ]
 then
@@ -26,5 +34,5 @@ fi
 rm -rf libtcl 
 
 git clone $uri libtcl
-(cd libtcl; git checkout tags/$1)
+(cd libtcl; git checkout $tag)
 (cd libtcl; autoreconf -i)
